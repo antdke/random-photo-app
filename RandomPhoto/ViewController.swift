35a3225efd4afc5b0c78be92bedd9e68f1955f47
Anthony Diké <anthonydike@nyu.edu>
2021-09-04 19:31:09 -0400

    feat: random image appears

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -15,12 +15,24 @@ class ViewController: UIViewController {
         imageView.backgroundColor = .white
         return imageView
     }()
+    
+    func getRandomPhoto() {
+        let urlString = "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemPink
         view.addSubview(imageView)
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        imageView.center = view.center
+        getRandomPhoto()
     }
 
 
